23ee3e79c28857789bb15685795bd0966e89241a
Satyam Jha <10satyam.jha@gmail.com>
2020-12-02 12:36:10 +0530

    new

--- front.py
+++ front.py
@@ -1,4 +1,4 @@
-from .wallpapaer import runtasks
+from wallpapaer import runtasks
 import schedule
 import time
 
@@ -6,7 +6,7 @@ email = "10satyam.jha@gmail.com"
 
 sub = "wallpapers"
 
-schedule.every().day.at("1:00").do(runtasks, sub=sub, email=email)
+schedule.every().day.at("10:00").do(runtasks, sub=sub, email=email)
 
 while True:
     schedule.run_pending()
